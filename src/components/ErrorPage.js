9ac467eeb76914372763e8ec01498092a89694bc
Nino Verhaegh <454777@student.fontys.nl>
2022-01-20 14:18:43 +0100

    fix

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -4,7 +4,7 @@ const ErrorPage = (props) => {
   return (
     <div>
       <center>
-        {/* Keep "Oops" in this component, testing purposes. */}
+        {/* Keep this phrase in this component, testing purposes. */}
         <h1>Oops, something went wrong</h1>
         <p>{props.message}</p>
       </center>
